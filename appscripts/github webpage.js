f4a7e6bcc4b7e5e56c63d40d1d1377727664eb1f
ongleejengjillian-nm2207 <111366670+ongleejengjillian-nm2207@users.noreply.github.com>
2023-03-20 02:07:55 +0800

    Update github webpage.js

--- appscripts/github webpage.js
+++ appscripts/github webpage.js
@@ -1,13 +1,13 @@
 //section 1:  Bar chart showing top 5 International Visitor Arrivals By Inbound Tourism Markets
 const myCountries = ["China", "India", "Indonesia", "Malaysia", "Bangladesh"];
-const myPercentage = ["88.24", "54.38", "33.45", "24.21", "17.89"];
+const mynNumbers = ["88240", "54380", "33450", "24210", "17890"];
 
 const dataObj = {
   labels: myCountries,
   datasets: [
       {
-          label: "Percentage of tourists",
-          data: myPercentage,
+          label: "number of tourists",
+          data: myNumbers,
           borderWidth: 2,
           backgroundColor: "hsla(300, 76%, 72%, 0.5)",
           borderColor: "hsla(333, 100%, 47%, 1)"
